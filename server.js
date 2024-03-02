e592430aea8a507f474f60a39bc6ee39d2ca0835
Lizbeth Noriega Gutierrez <lizbeth.noriega@utzmg.edu.mx>
2024-03-02 12:26:49 -0600

    Se agregó index

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send("Hello World")
+  res.sendFile(__dirname + "/index.html")
 })
 //ejemplo de función flecha 
 app.listen(3000, ()=>{ 
